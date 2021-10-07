56bc6d0f9770ce52dc4825b6ebbc075d13b2f9d9
syawaljasira <syawaljasira12@Gmail.com>
2021-10-07 14:54:03 +0700

    Improved and updated the calculator ui design

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -69,143 +69,145 @@ function Calculator() {
   };
 
   return (
-    <div>
-      <div className="row border border-dark">
-        <div className="col-12 d-flex justify-content-end bg-dark text-warning py-4 px-2">
-          <div className="row display">
-            <div id="display" class="col-11 text-end bg-dark">
-              {expression === '' ? 0 : expression}
-            </div>
-            <div className="col-11 text-end fs-4 text-light bg-dark">
-              {answer.length === 0 ? 0 : answer}
+    <div className="row text-grays">
+      <div className="col-10 col-sm-8 col-md-6 col-lg-5 mx-auto shadow-lg border-colored">
+        <div className="row shadow-lg">
+          <div className="col-12 d-flex justify-content-end bg-grad fs-1 text-whites py-4 px-2">
+            <div className="row display">
+              <div id="display" class="col-11 text-end">
+                {expression === '' ? 0 : expression}
+              </div>
+              <div className="col-11 text-end fs-4">
+                {answer.length === 0 ? 0 : answer}
+              </div>
             </div>
           </div>
-        </div>
-        <div
-          id="clear"
-          onClick={allClear}
-          className="col-3 p-3 border border-dark text-center fs-3 bg-danger"
-        >
-          ac
-        </div>
-        <div
-          id="c"
-          onClick={clear}
-          className="col-3 p-3 border border-dark text-center fs-3 bg-danger"
-        >
-          c
-        </div>
-        <div
-          id="divide"
-          onClick={() => display('/')}
-          className="col-3 p-3 border border-dark text-center fs-3 bg-secondary"
-        >
-          /
-        </div>
-        <div
-          id="multiply"
-          onClick={() => display('*')}
-          className="col-3 p-3 border border-dark text-center fs-3 bg-secondary"
-        >
-          *
-        </div>
-        <div
-          id="seven"
-          onClick={() => display('7')}
-          className="col-3 p-3 border border-dark text-center fs-3 bg-secondary"
-        >
-          7
-        </div>
-        <div
-          id="eight"
-          onClick={() => display('8')}
-          className="col-3 p-3 border border-dark text-center fs-3 bg-secondary"
-        >
-          8
-        </div>
-        <div
-          id="nine"
-          onClick={() => display('9')}
-          className="col-3 p-3 border border-dark text-center fs-3 bg-secondary"
-        >
-          9
-        </div>
-        <div
-          id="subtract"
-          onClick={() => display('-')}
-          className="col-3 p-3 border border-dark text-center fs-3 bg-secondary"
-        >
-          -
-        </div>
-        <div
-          id="four"
-          onClick={() => display('4')}
-          className="col-3 p-3 border border-dark text-center fs-3 bg-secondary"
-        >
-          4
-        </div>
-        <div
-          id="five"
-          onClick={() => display('5')}
-          className="col-3 p-3 border border-dark text-center fs-3 bg-secondary"
-        >
-          5
-        </div>
-        <div
-          id="six"
-          onClick={() => display('6')}
-          className="col-3 p-3 border border-dark text-center fs-3 bg-secondary"
-        >
-          6
-        </div>
-        <div
-          id="add"
-          onClick={() => display('+')}
-          className="col-3 p-3 border border-dark text-center fs-3 bg-secondary"
-        >
-          +
-        </div>
-        <div
-          id="one"
-          onClick={() => display('1')}
-          className="col-3 p-3 border border-dark text-center fs-3 bg-secondary"
-        >
-          1
-        </div>
-        <div
-          id="two"
-          onClick={() => display('2')}
-          className="col-3 p-3 border border-dark text-center fs-3 bg-secondary"
-        >
-          2
-        </div>
-        <div
-          id="three"
-          onClick={() => display('3')}
-          className="col-3 p-3 border border-dark text-center fs-3 bg-secondary"
-        >
-          3
-        </div>
-        <div
-          id="decimal"
-          onClick={() => display('.')}
-          className="col-3 p-3 border border-dark text-center fs-3 bg-secondary"
-        >
-          .
-        </div>
-        <div
-          id="zero"
-          onClick={() => display('0')}
-          className="col-6 p-3 border border-dark text-center fs-3 bg-secondary"
-        >
-          0
-        </div>
-        <div
-          id="equals"
-          onClick={calculate}
-          className="col-6 p-3 border border-dark text-center fs-3 bg-primary"
-        >
-          =
+          <div
+            id="clear"
+            onClick={allClear}
+            className="col-3 p-3 text-pink border-colored text-center fs-3 bg-gray"
+          >
+            ac
+          </div>
+          <div
+            id="c"
+            onClick={clear}
+            className="col-3 p-3 text-pink border-colored text-center fs-3 bg-gray"
+          >
+            c
+          </div>
+          <div
+            id="divide"
+            onClick={() => display('/')}
+            className="col-3 p-3 border-colored text-center fs-3 bg-gray"
+          >
+            /
+          </div>
+          <div
+            id="multiply"
+            onClick={() => display('*')}
+            className="col-3 p-3 border-colored text-center fs-3 bg-gray"
+          >
+            *
+          </div>
+          <div
+            id="seven"
+            onClick={() => display('7')}
+            className="col-3 p-3 border-colored text-center fs-3 bg-white"
+          >
+            7
+          </div>
+          <div
+            id="eight"
+            onClick={() => display('8')}
+            className="col-3 p-3 border-colored text-center fs-3 bg-white"
+          >
+            8
+          </div>
+          <div
+            id="nine"
+            onClick={() => display('9')}
+            className="col-3 p-3 border-colored text-center fs-3 bg-white"
+          >
+            9
+          </div>
+          <div
+            id="subtract"
+            onClick={() => display('-')}
+            className="col-3 p-3 border-colored text-center fs-3 bg-gray"
+          >
+            -
+          </div>
+          <div
+            id="four"
+            onClick={() => display('4')}
+            className="col-3 p-3 border-colored text-center fs-3 bg-white"
+          >
+            4
+          </div>
+          <div
+            id="five"
+            onClick={() => display('5')}
+            className="col-3 p-3 border-colored text-center fs-3 bg-white"
+          >
+            5
+          </div>
+          <div
+            id="six"
+            onClick={() => display('6')}
+            className="col-3 p-3 border-colored text-center fs-3 bg-white"
+          >
+            6
+          </div>
+          <div
+            id="add"
+            onClick={() => display('+')}
+            className="col-3 p-3 border-colored text-center fs-3 bg-gray"
+          >
+            +
+          </div>
+          <div
+            id="one"
+            onClick={() => display('1')}
+            className="col-3 p-3 border-colored text-center fs-3 bg-white"
+          >
+            1
+          </div>
+          <div
+            id="two"
+            onClick={() => display('2')}
+            className="col-3 p-3 border-colored text-center fs-3 bg-white"
+          >
+            2
+          </div>
+          <div
+            id="three"
+            onClick={() => display('3')}
+            className="col-3 p-3 border-colored text-center fs-3 bg-white"
+          >
+            3
+          </div>
+          <div
+            id="decimal"
+            onClick={() => display('.')}
+            className="col-3 p-3 border-colored text-center fs-3 bg-gray"
+          >
+            .
+          </div>
+          <div
+            id="zero"
+            onClick={() => display('0')}
+            className="col-6 p-3 border-colored text-center fs-3 bg-gray"
+          >
+            0
+          </div>
+          <div
+            id="equals"
+            onClick={calculate}
+            className="col-6 p-3 border-colored text-center fs-3 bg-grad"
+          >
+            =
+          </div>
         </div>
       </div>
     </div>
